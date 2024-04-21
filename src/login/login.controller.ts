27ace8a507ea438fa6ecf85c225354fd52523b81
Alessandro Dutra <cachopaweb@gmail.com>
2024-04-21 17:46:55 -0400

    bug fix

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -10,7 +10,7 @@ export class LoginController {
     async login(@Body() body: LoginDto) {
         const { email, senha } = body;
         try {
-            const usuario = this.prisma.usuarios.findFirstOrThrow({
+            const usuario = this.prisma.usuarios.findFirst({
                 where: {
                     email,
                     senha
