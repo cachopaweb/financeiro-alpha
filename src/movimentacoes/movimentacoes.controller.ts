b81f83f919932e192016611c54e5129ca7a3a7eb
Alessandro Dutra <cachopaweb@gmail.com>
2024-04-30 20:47:16 -0400

    add value default dataPrevisa in despesas and Receitas

--- src/movimentacoes/movimentacoes.controller.ts
+++ src/movimentacoes/movimentacoes.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { MovimentacoesService } from './movimentacoes.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('movimentacoes')
+@ApiTags('Movimentacoes')
 export class MovimentacoesController {
     constructor(private movimentacoesService: MovimentacoesService) { }
 
     @Get()
     async getMovimentacoes() {
-        return this.getMovimentacoes();
+        return this.movimentacoesService.getMovimentacoes();
     }
 }
