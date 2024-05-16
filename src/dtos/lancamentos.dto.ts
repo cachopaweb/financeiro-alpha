2cc204abaf5993e21f964855c76f3610961ba091
Alessandro Dutra <cachopaweb@gmail.com>
2024-05-15 20:43:00 -0400

    create lancamento receitas

--- src/dtos/lancamentos.dto.ts
+++ src/dtos/lancamentos.dto.ts
@@ -12,7 +12,7 @@ export class LancamentoDto {
 
     @ApiProperty()
     @IsNotEmpty()
-    despesaId: number;
+    recDesId: number;
 
     @ApiProperty()
     @IsNotEmpty()
--- src/lancamentos/lancamentos.controller.ts
+++ src/lancamentos/lancamentos.controller.ts
@@ -23,54 +23,112 @@ export class LancamentosController {
     }
 
     @Post()
-    async create(@Body() body: LancamentoDto) {
-        const { valor, obs, despesaId, userId, empresaId, tipo, dataHora } = body;
+    async create(@Body() lancamento: LancamentoDto) {
+        const { tipo } = lancamento;
         try {
-            const despesa = await this.prisma.despesas.findUnique({
-                where: {
-                    id: despesaId
-                }
-            })
-
-            if (!despesa) {
-                throw new Error('Despesa não encontrada')
+            if (tipo.includes('D')) {
+                return this.lancamentoDespesa(lancamento)
+            } else {
+                return this.lancamentoReceita(lancamento)
             }
-
-            const empresa = await this.prisma.empresas.findUnique({
-                where: {
-                    id: empresaId
-                }
-            })
-
-            if (!empresa) {
-                throw new Error('Empresa não encontrada')
-            }
-
-            const movimentacao = await this.movimentacoesService.insert({
-                dataHora,
-                credito: 0,
-                debito: valor,
-                descricao: `DES - ${despesa.nome}`
-            })
-
-            const diferenca = (parseFloat(despesa.valorEstimado.toString()) - valor);
-            const lancamentos = await this.prisma.lancamentos_receita_despesa.create({
-                data: {
-                    nome: despesa.nome,
-                    real: valor,
-                    obs,
-                    despesaId: despesa.id,
-                    estimado: despesa.valorEstimado,
-                    usuId: userId,
-                    diferenca,
-                    empresaId: empresa.id,
-                    tipo,
-                    movId: movimentacao.id
-                }
-            });
-            return lancamentos;
         } catch (error) {
             throw new HttpException(String(error), HttpStatus.BAD_REQUEST);
         }
     }
+
+
+    async lancamentoDespesa(lancamento: LancamentoDto) {
+        const { valor, obs, recDesId, userId, empresaId, tipo, dataHora } = lancamento;
+        const despesa = await this.prisma.despesas.findUnique({
+            where: {
+                id: recDesId
+            }
+        })
+
+        if (!despesa) {
+            throw new Error('Despesa não encontrada')
+        }
+
+        const empresa = await this.prisma.empresas.findUnique({
+            where: {
+                id: empresaId
+            }
+        })
+
+        if (!empresa) {
+            throw new Error('Empresa não encontrada')
+        }
+
+        const movimentacao = await this.movimentacoesService.insert({
+            dataHora,
+            credito: 0,
+            debito: valor,
+            descricao: `DES - ${despesa.nome}`
+        })
+
+        const diferenca = (parseFloat(despesa.valorEstimado.toString()) - valor);
+        const lancamentos = await this.prisma.lancamentos_receita_despesa.create({
+            data: {
+                nome: despesa.nome,
+                real: valor,
+                obs,
+                recDesId: despesa.id,
+                estimado: despesa.valorEstimado,
+                usuId: userId,
+                diferenca,
+                empresaId: empresa.id,
+                tipo,
+                movId: movimentacao.id
+            }
+        });
+        return lancamentos;
+    }
+
+    async lancamentoReceita(lancamento: LancamentoDto) {
+        const { valor, obs, recDesId, userId, empresaId, tipo, dataHora } = lancamento;
+        const receita = await this.prisma.receitas.findUnique({
+            where: {
+                id: recDesId
+            }
+        })
+
+        if (!receita) {
+            throw new Error('receita não encontrada')
+        }
+
+        const empresa = await this.prisma.empresas.findUnique({
+            where: {
+                id: empresaId
+            }
+        })
+
+        if (!empresa) {
+            throw new Error('Empresa não encontrada')
+        }
+
+        const movimentacao = await this.movimentacoesService.insert({
+            dataHora,
+            credito: valor,
+            debito: 0,
+            descricao: `REC - ${receita.nome}`
+        })
+
+        const diferenca = (parseFloat(receita.valorEstimado.toString()) - valor);
+        const lancamentos = await this.prisma.lancamentos_receita_despesa.create({
+            data: {
+                nome: receita.nome,
+                real: valor,
+                obs,
+                recDesId: receita.id,
+                estimado: receita.valorEstimado,
+                usuId: userId,
+                diferenca,
+                empresaId: empresa.id,
+                tipo,
+                movId: movimentacao.id
+            }
+        });
+        return lancamentos;
+    }
 }
+
